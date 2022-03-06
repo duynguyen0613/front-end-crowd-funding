5e3b582d6941e4c63a23eb8480b60115d4f4a4bf
Duy Nguyen <nguyendanh.duy9@gmail.com>
2022-03-06 14:03:02 -0500

    Modal for mobile version is finished

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const hamburgerUI = document.querySelector(".nav__hamburger__img img");
 
-hamburgerUI.addEventListener('click', function() {
+hamburgerUI.addEventListener('click', function () {
     console.log("helli")
     const navUI = document.querySelector(".nav");
 
@@ -8,7 +8,7 @@ hamburgerUI.addEventListener('click', function() {
 
 })
 
-window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function (e) {
     console.log(this.window.innerWidth)
     const width = this.window.innerWidth;
     // if window screen is resized then check if 
@@ -16,5 +16,37 @@ window.addEventListener('resize', function(e) {
     if (width > 810) {
         const nav = this.document.querySelector(".nav");
         nav.classList.remove("show");
+    }
+})
+
+
+const selectRewardButtons = document.querySelectorAll(".section__project__action a");
+
+var modalID;
+
+selectRewardButtons.forEach(b => {
+    b.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const main = document.querySelector("main");
+
+        modalID = e.target.dataset.target;
+        const modal = document.querySelector(modalID);
+        modal.classList.toggle("show");
+        main.style.display = "none";
+
+
+    })
+})
+
+const modalClose = document.querySelector("#modal--close");
+
+modalClose.addEventListener('click', function () {
+    const main = document.querySelector("main");
+    const modal = document.querySelector("#modal");
+
+    if (modal != null) {
+        modal.classList.toggle("show");
+        main.style.display = "unset";
     }
 })
